ab59596f7406d7171dda6da6ed7cbe376f998439
Natália <75141156+nataliakrein@users.noreply.github.com>
2021-05-27 23:43:39 -0300

    Delete main-community.js

--- main-community.js
+++
@@ -1,9 +0,0 @@
-const selectMenu = document.querySelector('.hamburguer');
-selectMenu.addEventListener('click', function(event) {
-    const menu = document.querySelector('.header__menu');
-    menu.classList.toggle('active');
-
-
-    const menuIcone = event.target;
-    menuIcone.setAttribute('class', menuIcone.classList.contains('fa-times') ? 'fas fa-bars hamburguer' : 'fas fa-times')
-})
